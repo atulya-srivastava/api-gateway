634323bf7619f86961874f6c585f044fb9b7baaf
atulya srivastava <vns.atulyasrivastava@gmail.com>
2026-07-28 21:33:27 +0530

    feat: implement routing logic to these services

--- main.go
+++ main.go
@@ -1,18 +1,30 @@
-package main
+	package main
 
-import (
-	"atulya/api-gateway/proxy"
-	"log"
-	"net/http"
-)
+	import (
+		"atulya/api-gateway/proxy"
+		"log"
+		"net/http"
+		"atulya/api-gateway/services"
+	)
 
-func main() {
+	func main() {
 
-	userProxy := proxy.NewProxy("http://localhost:8081")
+		// userProxy := proxy.NewProxy("http://localhost:8081")
+		// productProxy := proxy.NewProxy("http://localhost:8082")
+		// orderProxy := proxy.NewProxy("http://localhost:8083")
 
-	http.Handle("/users", userProxy)
+		// http.Handle("/users", userProxy)
+		// http.Handle("/products", productProxy)
+		// http.Handle("/orders",orderProxy)
 
-	log.Println("Gateway running on :8080")
+		for route, targetUrl := range services.Routes{
 
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
+			urlProxy := proxy.NewProxy(targetUrl)
+
+			http.Handle(route,urlProxy)
+		}
+
+		log.Println("Gateway running on :8080")
+
+		log.Fatal(http.ListenAndServe(":8080", nil))
+	}
---
+++ services/routes.go
@@ -0,0 +1,8 @@
+package services
+
+
+var Routes = map[string] string{
+	"/users":    "http://localhost:8081",
+	"/products": "http://localhost:8082",
+	"/orders":   "http://localhost:8083",
+}
