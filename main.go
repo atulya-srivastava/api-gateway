60c088b554daafe7b8c1e0f481829afed891fbae
atulya srivastava <vns.atulyasrivastava@gmail.com>
2026-08-22 23:54:57 +0530

    refactor: cleanup main.go

--- main.go
+++ main.go
@@ -11,19 +11,10 @@ import (
 
 	func main() {
 
-		// userProxy := proxy.NewProxy("http://localhost:8081")
-		// productProxy := proxy.NewProxy("http://localhost:8082")
-		// orderProxy := proxy.NewProxy("http://localhost:8083")
-
-		// http.Handle("/users", userProxy)
-		// http.Handle("/products", productProxy)
-		// http.Handle("/orders",orderProxy)
-
 		for route, targetUrls := range services.Routes{
 
 			healthyServers := healthcheck.CheckAll(targetUrls)
-
-			// If no backend is healthy, skip this route
+			
 			if len(healthyServers) == 0 {
 				log.Println("No healthy servers for", route)
 			}
